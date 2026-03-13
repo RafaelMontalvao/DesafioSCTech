ca0e83da2462a0b567b467af91d00a62c7aad1c3
Rafael Brito <rushbig@hotmail.com>
2026-03-13 15:18:09 -0300

    criando o backend

---
+++ backend/empreendimentos-api/src/main/java/br/sc/sctec/empreendimentos_api/EmpreendimentosApiApplication.java
@@ -0,0 +1,13 @@
+package br.sc.sctec.empreendimentos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpreendimentosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpreendimentosApiApplication.class, args);
+	}
+
+}
---
+++ backend/empreendimentos-api/src/test/java/br/sc/sctec/empreendimentos_api/EmpreendimentosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.sc.sctec.empreendimentos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpreendimentosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
